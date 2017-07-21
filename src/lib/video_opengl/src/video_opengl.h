1aa7ff9e83c09f7702b052aef38f818cda5cfbe0
Bailey Stoner <bailey@patreon.com>
2017-07-20 19:48:13 -0700

    Move video_opengl to https://github.com/hapengine/video_opengl

--- src/lib/video_opengl/src/video_opengl.h
+++
@@ -1,7 +0,0 @@
-#include <hap.h>
-
-void* create(HAPEngine *engine);
-void load(HAPEngine *engine, void *state, char *identifier);
-HAPTime update(HAPEngine *engine, void *state);
-void unload(HAPEngine *engine, void *state);
-void destroy(HAPEngine *engine, void *state);
